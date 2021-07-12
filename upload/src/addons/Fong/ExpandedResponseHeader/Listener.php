62bbc25953dda1eef8b84e4c15ccfffaf149b7ad
Jayson Fong <fong.jayson@gmail.com>
2021-07-12 14:43:49 -0400

    Minor Cleanup

--- upload/src/addons/Fong/ExpandedResponseHeader/Listener.php
+++ upload/src/addons/Fong/ExpandedResponseHeader/Listener.php
@@ -4,28 +4,28 @@
 namespace Fong\ExpandedResponseHeader;
 
 
+use XF;
+use XF\Http\Response;
+use XF\Mvc\Dispatcher;
+use XF\Mvc\Renderer\AbstractRenderer;
+use XF\Mvc\Reply\AbstractReply;
+
 class Listener
 {
 
     /**
-     * @param \XF\Mvc\Dispatcher $dispatcher
+     * @param Dispatcher $dispatcher
      * @param $content
-     * @param \XF\Mvc\Reply\AbstractReply $reply
-     * @param \XF\Mvc\Renderer\AbstractRenderer $renderer
-     * @param \XF\Http\Response $response
+     * @param AbstractReply $reply
+     * @param AbstractRenderer $renderer
+     * @param Response $response
      */
-    public static function addData(
-        \XF\Mvc\Dispatcher $dispatcher,
-        &$content,
-        \XF\Mvc\Reply\AbstractReply $reply,
-        \XF\Mvc\Renderer\AbstractRenderer $renderer,
-        \XF\Http\Response $response
-    )
+    public static function addData(Dispatcher $dispatcher, &$content, AbstractReply $reply, AbstractRenderer $renderer, Response $response)
     {
-        $visitor = \XF::visitor();
+        $visitor = XF::visitor();
 
-        // Set Headers
-        if (\XF::options()->expandedresponseheader_headers) {
+        // Set Response Headers
+        if (XF::options()->expandedresponseheader_headers) {
             $response->header('Xf-User-Id', (int) $visitor->user_id);
             $response->header('Xf-Is-Staff', (int) $visitor->is_staff);
             $response->header('Xf-Is-Banned', (int) $visitor->is_banned);
